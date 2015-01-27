a7ad965bb666fe837637d36687035e884100bf27
timsvoice <timsethvoice@gmail.com>
2015-01-27 18:09:29 -0500

    added ng-app

---
+++ public/scripts/scripts.f8e89a18.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("ngAppApp",["ngAnimate","ngCookies","ngResource","ngRoute","ngSanitize","ngTouch"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("ngAppApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
